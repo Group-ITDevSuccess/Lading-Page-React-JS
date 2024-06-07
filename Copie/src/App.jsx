dc718ff4a37a147402d19b2666d9e8dd217fe4aa
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-07 21:50:24 +0300

    Ajout de Features

--- Copie/src/App.jsx
+++ Copie/src/App.jsx
@@ -3,6 +3,7 @@ import JsonData from './data/data.json'
 import SmoothScroll from "smooth-scroll"
 import {Navigation} from "./components/Navigation";
 import {Header} from "./components/Header";
+import {Features} from "./components/Features";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000,
@@ -20,6 +21,7 @@ const App = () => {
         <div>
             <Navigation/>
             <Header data={landingPageData.Header} />
+            <Features data={landingPageData.Features} />
         </div>
     )
 }
