13383944bf8ffecfa952f47e932d42663cf1615b
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-08 05:49:44 +0300

    Ajout de Testimonials

--- Copie/src/App.jsx
+++ Copie/src/App.jsx
@@ -7,6 +7,7 @@ import {Features} from "./components/Features";
 import {About} from "./components/About";
 import {Services} from "./components/Services";
 import {Gallery} from "./components/Gallery";
+import {Testimonials} from "./components/Testimonials";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000,
@@ -28,6 +29,7 @@ const App = () => {
             <About data={landingPageData.About} />
             <Services data={landingPageData.Services} />
             <Gallery data={landingPageData.Gallery} />
+            <Testimonials data={landingPageData.Testimonials} />
         </div>
     )
 }
